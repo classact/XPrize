3148e37dd40b6f48f93b82e32cf07e92ac7d2ca3
Mediaetc <dave@mediaetc.co.za>
2017-02-23 22:11:31 +0200

    Swahili fix (still in progress)

--- app/src/main/java/classact/com/xprize/MainActivity.java
+++ app/src/main/java/classact/com/xprize/MainActivity.java
@@ -29,13 +29,13 @@ import classact.com.xprize.utils.ResourceDecoder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final boolean ALLOW_DB_RECOPY = false;
+    private final boolean ALLOW_DB_RECOPY = true;
 
     // Database hack related
-    private final boolean HACK_NEXT_UNIT = false;
+    private final boolean HACK_NEXT_UNIT = true;
     private final int HACK_UNIT_ID = 1;
     private final int HACK_UNIT_SUB_ID_IN_PROGRESS = 0;
-    private final int HACK_DRILL_LAST_PLAYED = 0;
+    private final int HACK_DRILL_LAST_PLAYED = 4;
     private final int HACK_UNIT_FIRST_TIME = 0;
     private final int HACK_UNIT_FIRST_TIME_MOVIE = 1;
 
